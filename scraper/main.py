67c1cae1195bb374920dfa01e72a8ba37bae1ad9
Eric Lingren <ericlingren@gmail.com>
2020-08-18 21:53:21 -0600

    code cleanup, looping possible

--- scraper/main.py
+++ scraper/main.py
@@ -1,18 +1,26 @@
 from datetime import date
 from scraper import initilize_scraper
 
-currency = 'AUDUSD'
+
+# Declare config caribles here before running script
+    # Change the output_path to the path you want the file to save in
+    # If None is used for the output_path the file will download in the directory where the script lives
+
+# output_path = '/Users/ericlingren/Desktop/'  
+output_path = None  
+currency = 'AUDCAD'
 start_year =  2020
-start_month =  2
-start_day = 1
+start_month =  7
+start_day = 3
 end_year =  2020
-end_month =  3
-end_day =  7
+end_month =  7
+end_day =  3
 
 def run_main():
     initilize_scraper(
+        output_path,
         currency,  
-        date(start_year , start_month, start_day), 
+        date(start_year, start_month, start_day), 
         date(end_year, end_month, end_day) )
 
 run_main()
--- scraper/scraper.py
+++ scraper/scraper.py
@@ -2,28 +2,30 @@ import duka
 import asyncio
 import requests
 from datetime import date, timedelta
-
-def initilize_scraper(currency, start_date, end_date):
-    print(currency)
-    print(start_date)
-    print(end_date)
-
-# url = "https://datafeed.dukascopy.com/datafeed/AUDCAD/2020/07/03/04h_ticks.bi5"
+import os.path
+# url = "https://datafeed.dukascopy.com/datafeed/AUDCAD/2020/07/03/04h_ticks.bi5" # WORKING EXAMPLE PATH
 url = 'https://datafeed.dukascopy.com/datafeed/PAIR/YYYY/MM/DD/HHh_ticks.bi5'
+data_urls = []
+requested_dates = []
 
-urls = []
 
+
+def initilize_scraper(requested_output_path, requested_currency, start_date, end_date):
+    global output_path
+    output_path = requested_output_path
+    global currency;
+    currency = requested_currency
+    build_dates(start_date, end_date)
+
+
+# Calls the build_url function for each day of data requested
 def compile_dates(requested_dates):
-    # print(requested_dates) 
     for date in requested_dates:
-        # print(date)
-        build_url('AUDUSD', date)
-    print(urls)
+        build_url(currency, date)
+    get_data()
 
 
-
-
-# def build_url(pair, year, month, day):
+# Builds the 24 unique urls needed for each day of tick data and saves those in the data_urls array 
 def build_url(pair, date):
     year = f'{date.year}'
     month = f'{date.month}' if date.month > 9 else f'0{date.month}' 
@@ -39,32 +41,19 @@ def build_url(pair, date):
         new_url = new_url.replace('MM', month)
         new_url = new_url.replace('DD', day)
         new_url = new_url.replace('HH', hour)
-        urls.append(new_url)
-    
-
-# build_url('AUDCAD', '2020', '07', '03' )
+        data_urls.append(new_url)
 
 
-
-
-
-requested_dates = []
-
+#  Collates all the dates that will be required to build the array of urls
 def build_dates(start_date, end_date):
     delta = end_date - start_date       
     for i in range(delta.days + 1):
         day = start_date + timedelta(days=i)
-        print(day)
         requested_dates.append(day)
     compile_dates(requested_dates)   
 
-# build_dates( date(2020, 2, 1), date(2020, 2, 1) )
 
-
-
-
-
-
+#  Builds a dynamic file name for the data that will be downloaded
 def generate_file_name(url):
     pair = url.split('/')[-5]
     year = url.split('/')[-4]
@@ -72,22 +61,26 @@ def generate_file_name(url):
     day = url.split('/')[-2]
     hour = url.split('/')[-1]
     name = f'{pair}-{year}-{month}-{day}-{hour}'
-    return name
+    complete_name = None
+    if output_path != None:
+        complete_name = os.path.join(output_path, name)
+    else:
+        complete_name = name  
+    return complete_name
 
 
+# Data Fetcher
 def get_data():
-    file_name = generate_file_name(url)
-    try:
-        res = requests.get(url, allow_redirects=True)
-        if res.status_code == 200:
-            with open(file_name, 'wb') as fd:
-                for chunk in res.iter_content(chunk_size=128):
-                    fd.write(chunk)
-    except:
-        print('ERROR - FAILED')
-
-# get_data()
-
-
-
-# generate_file_name()
+    for url in data_urls:
+        file_name = generate_file_name(url)
+        print(file_name)
+        try:
+            res = requests.get(url, allow_redirects=True)
+            if res.status_code == 200:
+                print('success')
+                with open(file_name, 'w') as fd:
+                    for chunk in res.iter_content(chunk_size=128):
+                        fd.write(chunk)
+            else: print(res.status_code)
+        except:
+            print('ERROR - FAILED')
