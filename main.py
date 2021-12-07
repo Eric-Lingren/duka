e7682d638d700e37333368cbc77818e41e874aed
Eric Lingren <ericlingren@gmail.com>
2021-12-06 20:56:56 -0800

    code sync

--- main.py
+++ main.py
@@ -7,17 +7,18 @@ from converters import integrated_csv_to_hdf5
 # Duka URL Tags
 # XAGUSD XAUUSD BRENTCMDUSD GASCMDUSD SOYBEANCMDUSX SUGARCMDUSD BTCUSD
 
-pair = 'AUDJPY'
-# years = ['2020', '2019', '2018', '2017', '2016']
-years = ['2015', '2014', '2013', '2012', '2011', '2010']
+pair = 'SGDJPY'
+years = ['2020', '2019', '2018', '2017', '2016']
+# years = ['2015', '2014', '2013', '2012', '2011', '2010']
 # years = ['2009', '2008', '2007', '2006', '2005']
 # years = ['2009', '2008']
 # years = ['2016']
 # years = ['2021']
 # years = ['2018', '2017', '2016']
-# dir_path = '/Volumes/External/Trading/historical-data/forex'
+# years = ['2012', '2011', '2010']
+dir_path = '/Volumes/External/Trading/historical-data/forex'
 # dir_path = '/Volumes/External/Trading/historical-data/commodities'
-dir_path = '/Volumes/External/Trading/historical-data/crypto'
+# dir_path = '/Volumes/External/Trading/historical-data/crypto'
 # dir_path = '/Users/ericlingren/Desktop'
 
 
